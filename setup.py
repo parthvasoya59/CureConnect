dc1162b93c18a162932a8db37d6886732212cd9d
parthvasoya59 <parthvasoya1000@gmail.com>
2024-05-07 16:20:11 -0400

    frontend added and flask

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Cure Connect Chatbot',
+    version= '0.0.0',
+    author= 'Parth Vasoya',
+    author_email= 'parthvasoya1000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
